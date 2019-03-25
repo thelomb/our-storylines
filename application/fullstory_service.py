88350b49370b94fed60ab047a3f94f1a19be12c9
Pascal Lombardet <thelomb@gmail.com>
2019-03-25 10:13:39 +0100

    fullstory with image

--- application/fullstory_service.py
+++ application/fullstory_service.py
@@ -28,18 +28,21 @@ class Fullstory():
         self.author = author
         self.files = files
         self.story = story
-        self.media_request()
+        self.media = []
+        if self.files:
+            self.process_media_files()
 
     def store(self):
         Story.create(date_for=self.date_for,
                      title=self.title,
                      content=self.content,
                      user_id=self.author.id,
-                     media=self.media['photo'])
+                     media=self.media)
         print('story created!')
 
     def update(self, date_for, title, content, start, end, stay,
                odometer_at, travel_type, author, files):
+        self.media = self.story.media
         self.title = title
         self.content = content
         self.start = start
@@ -49,38 +52,28 @@ class Fullstory():
         self.travel_type = travel_type
         self.author = author
         self.files = files
-        self.media_request(self.story.media)
+        if self.files:
+            self.process_media_files()
         self.story.update(date_for=self.date_for,
                           title=self.title,
                           content=self.content,
-                          user_id=self.author.id,
-                          media=self.media['photo'])
-        print('story updated!')
+                          user_id=self.author.id)
 
-    def media_request(self, existing_media=None):
-            photos = []
-            # audios = []
-            # videos = []
-            media = {
-                'photo': [],
-                # 'video': [],
-                # 'audio': []
-            }
-            if existing_media:
-                for existing_medium in existing_media:
-                    photos.append(existing_medium)
-
-            if self.files:
-                for image_info in self.files:
+    def process_media_files(self):
+        photo = []
+        if self.files:
+            for image_info in self.files:
                     filename = images.save(image_info)
                     url = images.url(filename)
                     image = Media(name=filename,
                                   filename=filename,
                                   url=url,
                                   type='Image')
-                    photos.append(image)
-            media['photo'] = photos
-            self.media = media
+                    photo.append(image)
+            if self.media:
+                self.media.extend(photo)
+            else:
+                self.media = photo
 
     @classmethod
     def get_by_date(cls, date_for):

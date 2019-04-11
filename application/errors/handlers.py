6328e47df6086e6ff6ba8b4e725ac5634ad1eeb2
Pascal Lombardet <thelomb@gmail.com>
2019-04-11 16:10:21 +0200

    typo in error handler

--- application/errors/handlers.py
+++ application/errors/handlers.py
@@ -3,12 +3,12 @@ from application import db
 from application.errors import bp
 
 
-@bp.errorhandler(404)
-def not_found_error(error):
+@bp.app_errorhandler(404)
+def page_not_found(error):
     return render_template('errors/404.html'), 404
 
 
-@bp.errorhandler(500)
+@bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
     return render_template('errors/500.html'), 500
